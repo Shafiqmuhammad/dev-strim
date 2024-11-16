a440e5e4e0acf3cae114b4e0e7f65a6ebdd26ce5
Shafiqmuhammad <115002187+Shafiqmuhammad@users.noreply.github.com>
2024-11-17 00:50:43 +0500

    updated

--- src/app/data/teams.ts
+++ src/app/data/teams.ts
@@ -9,15 +9,6 @@ export const packages = [
         prices: ""
     },
     {
-        id: 1,
-        url: "/teams/muzharhussain.jfif",
-        // url: "/assets/packages/nephrite-jade-al-hamid-minerals.jpg",
-        alt: "Nephrite Jade 1",
-        heading: "Muzhar Hussain",
-        desc: "Software Architect | Solopreneur | Web3 | Laravel | Python | Next.js | Linux | JAMstack Engineer",
-        prices: ""
-    },
-    {
         id: 2,
         url: "/teams/muzharhussain.jfif",
         // url: "/assets/packages/nephrite-jade-al-hamid-minerals.jpg",
@@ -28,6 +19,15 @@ export const packages = [
     },
     {
         id: 3,
+        url: "/teams/muzharhussain.jfif",
+        // url: "/assets/packages/nephrite-jade-al-hamid-minerals.jpg",
+        alt: "Nephrite Jade 1",
+        heading: "Muzhar Hussain",
+        desc: "Software Architect | Solopreneur | Web3 | Laravel | Python | Next.js | Linux | JAMstack Engineer",
+        prices: ""
+    },
+    {
+        id: 4,
         url: "/teams/muzharhussain.jfif",
         // url: "/assets/packages/nephrite-jade-al-hamid-minerals.jpg",
         alt: "Nephrite Jade 1",
