490921e65f21090650eb52d245d5bab5a18a6e7b
Shafiqmuhammad <115002187+Shafiqmuhammad@users.noreply.github.com>
2024-11-17 21:08:21 +0500

    updated

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,15 +1,30 @@
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//     user: process.env.PGUSER,
+//     password: process.env.PGPASSWORD,
+//     host: process.env.PGHOST,
+//     //   port: process.env.PGPORT,
+//     database: process.env.PGDATABASE,
+//     ssl: true,
+// });
+// // const pool = new Pool({
+// //     connectionString: process.env.DATABASE_URL
+// // });
+
+// export default pool
+
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    //   port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
+    user: process.env.PGUSER as string,
+    password: process.env.PGPASSWORD as string,
+    host: process.env.PGHOST as string,
+    database: process.env.PGDATABASE as string,
     ssl: true,
 });
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL
-// });
 
-export default pool
+export default pool;
